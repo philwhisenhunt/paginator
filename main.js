38c138067e20725798dcd5a1eb2841bcc6702699
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-10 12:10:01 -0500

    adding rest of files

---
+++ main.js
@@ -0,0 +1,78 @@
+const Apify = require('apify');
+
+Apify.main(async () => {
+    // Get the queue and start with the first one
+    const requestQueue = await Apify.openRequestQueue();
+    //Puts RVA link in the queue
+    await requestQueue.addRequest(new Apify.Request({url: 'https://www.visitrichmondva.com/event/'}));
+
+    //makes a new PuppeteerCrawler Object
+    const crawler = new Apify.PuppeteerCrawler({
+        requestQueue,
+
+        handlePageFunction: async ({ page, request }) => {
+            let event = {
+                url: null,
+                name: null,
+                address: null,
+                dates: null,
+                reccurence: null,
+                venue: null,
+                phone: null,
+                time: null,
+                price: null,
+                website: null,
+            }
+            //
+            event.url = request.url;
+            event.name = await page.$eval('div.detailInfo div.inner h1', h1 => h1.innerText);
+            // event.address = await page.$eval('div.detailInfo div.inner ul.info-list li[data-name="address"]', li =>li.innerText);
+            // event.dates = await page.$eval('div.detailInfo div.inner ul.info-list li[data-name="dates"]', li =>li.innerText);
+            // event.reccurence = await page.$eval('div.detailInfo div.inner ul.info-list li[data-name="recur"]', li =>li.innerText);
+
+            // Find the link to the next page using Puppeteer functions.
+            let nextHref;
+            try {
+                nextHref = await page.$eval('div.listingContainerFoot div.eventPagerBottom div.listFilters div.map-pager div.pagingFilterContainer div.listingPagerContainer a[class="nxt"]', el => el.href);
+            } catch (err) {
+                console.log(`${request.url} is the last page!`);
+                return;
+            }
+
+            // Enqueue the link to the RequestQueue
+            await requestQueue.addRequest(new Apify.Request({ url: nextHref }));
+        
+
+
+            //target the li with the data-name:address
+            
+            //add more selectors here
+            console.log(event);
+        },
+        
+
+        
+
+
+        //If fail 4 times, then execute this:
+        handleFailedRequestFunction: async ({ request }) => {
+            console.log(`Request ${request.url} failed 4 times`);
+        },
+        
+    });
+    
+   
+    
+
+
+
+    //Run crawler.
+    await crawler.run();
+
+
+});
+
+
+
+//class .nxt is what we are looking for when wanting to 
+

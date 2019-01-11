6e73ce9557dc10e33b8c13b4e5a1de5251cd9a8a
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-11 11:37:22 -0500

    tried some things, but they didn't work, commenting them out and cleaning up

--- main.js
+++ main.js
@@ -26,27 +26,34 @@ Apify.main(async () => {
                 website: null,
             }
 
+            
+
+            // event.forEach(function(element){
+            //     console.log(element);
+            // });
+            //for each event-link here.. open the page and log it. 
+
             //trying out a forEach here
 
               // A function to be evaluated by Puppeteer within the browser context.
-              const pageFunction = ($posts) => {
-                const data = [];
+            //   const pageFunction = ($posts) => {
+            //     const data = [];
 
-                // We're trying to get the title of each event on the main page.
-                $posts.forEach(($post) => {
-                    data.push({
-                        title: $post.querySelector('div.inner h4').innerText,
+            //     // We're trying to get the title of each event on the main page.
+            //     $posts.forEach(($post) => {
+            //         data.push({
+            //             title: $post.querySelector('div.inner h4').innerText,
                       
-                    });
-                });
-                console.log("First: " + data);
+            //         });
+            //     });
+            //     console.log("First: " + data);
 
-                return data;
-            };
-            const data = await page.$$eval('div.inner h4', pageFunction);
-            console.log("Second: " + data);
+            //     return data;
+            // };
+            // const data = await page.$$eval('div.inner h4', pageFunction);
+            // console.log("Second: " + data);
 
-            await Apify.pushData(data);
+            // await Apify.pushData(data);
 
 
 
@@ -81,6 +88,44 @@ Apify.main(async () => {
         },
         
     });
+
+    //Add the other crawler here? Can you use the same name? 
+
+    const crawler2 = new Apify.PuppeteerCrawler({
+        requestQueue,
+
+        handlePageFunction: async ({ page, request }) => {
+            let event = {
+                url: null,
+                name: null,
+                address: null,
+                dates: null,
+                reccurence: null,
+                venue: null,
+                phone: null,
+                time: null,
+                price: null,
+                website: null,
+            }
+            //
+            event.url = request.url;
+            event.name = await page.$eval('div.detailInfo div.inner h1', h1 => h1.innerText);
+            event.address = await page.$eval('div.detailInfo div.inner ul.info-list li[data-name="address"]', li =>li.innerText);
+            event.dates = await page.$eval('div.detailInfo div.inner ul.info-list li[data-name="dates"]', li =>li.innerText);
+            event.reccurence = await page.$eval('div.detailInfo div.inner ul.info-list li[data-name="recur"]', li =>li.innerText);
+
+
+            //target the li with the data-name:address
+            
+            //add more selectors here
+            console.log(event);
+        },
+
+        //If fail 4 times, then execute this:
+        handleFailedRequestFunction: async ({ request }) => {
+            console.log(`Request ${request.url} failed 4 times`);
+        },
+    });
     
    
     
@@ -89,7 +134,7 @@ Apify.main(async () => {
 
     //Run crawler.
     await crawler.run();
-
+    await crawler2.run();
 
 });
 
